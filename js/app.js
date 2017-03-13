84c4b4af7d83129913bec761e40296377fde222c
Yoscroll <ianhenry.c808@gmail.com>
2017-03-13 08:26:34 -1000

    k

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("sanity check");
+//Finding HTML elements
+//console.log(document.getElementById('use-id-method'));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+
+console.log(document.getElementsByTagName('script'));
+var tagElement = document.getElementsByTagName('script');
+console.log(tagElement);
