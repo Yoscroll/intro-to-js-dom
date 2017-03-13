b0aeffd92e1799f92be175469bb7a8bcf868eb59
Yoscroll <ianhenry.c808@gmail.com>
2017-03-13 08:44:52 -1000

    working

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 console.log("sanity check");
 //Finding HTML elements
+
+//ID
 //console.log(document.getElementById('use-id-method'));
 var idElement = document.getElementById("use-id-method");
 console.log(idElement);
 
+//TAG
 //console.log(document.getElementsByTagName('script'));
 var tagElement = document.getElementsByTagName('script');
 console.log(tagElement);
 
+//CLASS
 //console.log(document.getElementsByClass('use-class-method'));
 var classElement = document.getElementsByClassName('use-class-method');
-console.log(classElement[0]);
-console.log(classElement[1]);
+console.log(classElement);
+
+//changing HTML elements
+
+//ID
+console.log(idElement.innerHTML);
+idElement.innerHTML="I have been changed";
+
+//chnage second HTML element with class
+console.log(classElement[1].innerHTML)
+classElement[1].innerHTML = "I have been changed by class name and array location";
+
